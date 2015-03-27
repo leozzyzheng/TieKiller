89a911454011bd38c91a66750301a8a5f2712275
leozzyzheng <z175269158@hotmail.com>
2015-03-27 16:56:25 +0800

    更改下拉刷新的布局

--- src/com/leozzyzheng/tiekiller/ui/ForumActivity.java
+++ src/com/leozzyzheng/tiekiller/ui/ForumActivity.java
@@ -18,20 +18,24 @@ import com.leozzyzheng.tiekiller.ui.adapter.SimplePageAdapter;
 import com.leozzyzheng.tiekiller.ui.base.BaseActivity;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by leozzyzheng on 2015/3/26.
  * 喜欢的吧，有权限的吧的界面
  */
 public class ForumActivity extends BaseActivity {
-    private SwipeRefreshLayout swipeView;
+    private Map<Integer, SwipeRefreshLayout> swipeViewMap = new HashMap<Integer, SwipeRefreshLayout>();
     private ListView likeForumListView;
     private ViewPager pagers;
     private LikeForumListAdapter likeForumAdapter;
     private ListView baWuForumListView;
     private BaWuForumAdapter baWuForumAdapter;
     private SearchView baWuSearchView;
+
+    private String cachedSearchWord;
 
     public static final int LIKE_FORUM_INDEX = 0;
     public static final int BAWU_FORUM_INDEX = 1;
@@ -40,26 +44,37 @@ public class ForumActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forum);
-        swipeView = (SwipeRefreshLayout) findViewById(R.id.swipe);
-        swipeView.setColorSchemeResources(R.color.holo_blue_dark, R.color.holo_blue_light, R.color.holo_green_light, R.color.holo_green_light);
-        swipeView.setOnRefreshListener(mRefreshListener);
-
         pagers = (ViewPager) findViewById(R.id.pagers);
 
         View likeForumView = getLayoutInflater().inflate(R.layout.list_like_forum, pagers, false);
         View baWuForumView = getLayoutInflater().inflate(R.layout.list_bawu_forum, pagers, false);
 
-        likeForumListView = (ListView) likeForumView.findViewById(R.id.like_forum_list_view);
-        baWuForumListView = (ListView) baWuForumView.findViewById(R.id.bawu_list_view);
-
-        baWuSearchView = (SearchView) baWuForumView.findViewById(R.id.search_view);
-        baWuSearchView.setOnQueryTextListener(mBaWuSearchViewListener);
+        initLikeForum(likeForumView);
+        initBaWuForum(baWuForumView);
 
         List<View> list = new ArrayList<View>();
         list.add(LIKE_FORUM_INDEX, likeForumView);
         list.add(BAWU_FORUM_INDEX, baWuForumView);
         pagers.setAdapter(new SimplePageAdapter(list));
         pagers.setOnPageChangeListener(mPagerChangeListener);
+    }
+
+    private void initLikeForum(View likeForumView) {
+        likeForumListView = (ListView) likeForumView.findViewById(R.id.like_forum_list_view);
+        SwipeRefreshLayout likeForumSwipeView = (SwipeRefreshLayout) likeForumView.findViewById(R.id.swipe);
+        likeForumSwipeView.setColorSchemeResources(R.color.holo_blue_dark, R.color.holo_blue_light, R.color.holo_green_light, R.color.holo_green_light);
+        likeForumSwipeView.setOnRefreshListener(mRefreshListener);
+        swipeViewMap.put(LIKE_FORUM_INDEX, likeForumSwipeView);
+    }
+
+    private void initBaWuForum(View baWuForumView) {
+        baWuForumListView = (ListView) baWuForumView.findViewById(R.id.bawu_list_view);
+        SwipeRefreshLayout baWuForumSwipeView = (SwipeRefreshLayout) baWuForumView.findViewById(R.id.swipe);
+        baWuForumSwipeView.setColorSchemeResources(R.color.holo_blue_dark, R.color.holo_blue_light, R.color.holo_green_light, R.color.holo_green_light);
+        baWuForumSwipeView.setOnRefreshListener(mRefreshListener);
+        swipeViewMap.put(BAWU_FORUM_INDEX, baWuForumSwipeView);
+        baWuSearchView = (SearchView) baWuForumView.findViewById(R.id.search_view);
+        baWuSearchView.setOnQueryTextListener(mBaWuSearchViewListener);
     }
 
     /**
@@ -76,12 +91,12 @@ public class ForumActivity extends BaseActivity {
                 likeForumAdapter.notifyDataSetInvalidated();
             }
 
-            swipeView.setRefreshing(false);
+            swipeViewMap.get(LIKE_FORUM_INDEX).setRefreshing(false);
         }
 
         @Override
         public void onFailed() {
-            swipeView.setRefreshing(false);
+            swipeViewMap.get(LIKE_FORUM_INDEX).setRefreshing(false);
         }
     };
 
@@ -91,6 +106,13 @@ public class ForumActivity extends BaseActivity {
             if (pagers.getCurrentItem() == LIKE_FORUM_INDEX) {
                 RecommendedForumRequest request = new RecommendedForumRequest(mLikeForumListener);
                 request.send();
+            } else {
+                if (cachedSearchWord != null) {
+                    BaWuForumRequest request = new BaWuForumRequest(cachedSearchWord, mBaWuSearchListener);
+                    request.send();
+                } else {
+                    swipeViewMap.get(pagers.getCurrentItem()).setRefreshing(false);
+                }
             }
         }
     };
@@ -98,6 +120,8 @@ public class ForumActivity extends BaseActivity {
     private SearchView.OnQueryTextListener mBaWuSearchViewListener = new SearchView.OnQueryTextListener() {
         @Override
         public boolean onQueryTextSubmit(String query) {
+            cachedSearchWord = query;
+            swipeViewMap.get(BAWU_FORUM_INDEX).setRefreshing(true);
             BaWuForumRequest request = new BaWuForumRequest(query, mBaWuSearchListener);
             request.send();
             return true;
@@ -120,32 +144,28 @@ public class ForumActivity extends BaseActivity {
                 baWuForumAdapter.setData(forumListData.getForumList());
                 baWuForumAdapter.notifyDataSetInvalidated();
             }
+
+            swipeViewMap.get(BAWU_FORUM_INDEX).setRefreshing(false);
         }
 
         @Override
         public void onFailed() {
             Toast.makeText(ForumActivity.this, "没有根据输入查到有权限的贴吧", Toast.LENGTH_SHORT).show();
+            swipeViewMap.get(BAWU_FORUM_INDEX).setRefreshing(false);
         }
     };
 
     private ViewPager.OnPageChangeListener mPagerChangeListener = new ViewPager.OnPageChangeListener() {
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
         }
 
         @Override
         public void onPageSelected(int position) {
-            if (position == BAWU_FORUM_INDEX) {
-                swipeView.setEnabled(false);
-            } else {
-                swipeView.setEnabled(true);
-            }
         }
 
         @Override
         public void onPageScrollStateChanged(int state) {
-
         }
     };
 
